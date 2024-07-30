6fb1d53c27cd8f84916c84ef7387759d8058cfb2
Kipkosgeii <odkhoder@gmail.com>
2024-07-30 08:49:01 +0100

    'The top-paying job & skill'

---
+++ project_sql/1_top_paying_job.sql
@@ -0,0 +1,6 @@
+/* 
+ Question: What are the most top-paying data analyst jobs?
+ -Identify the top highest-paying Data analyst roles that are availabe remotely
+ - focus on the jib posting with specific salaries 
+ - why Highlight the top-paying opportynities for data analys offering insights into employment 
+ */
