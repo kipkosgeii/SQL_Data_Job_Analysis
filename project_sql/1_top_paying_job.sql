0bc7c932a6952dccfc41199fc20a66e7c0301a34
Kipkosgeii <odkhoder@gmail.com>
2024-07-30 16:55:09 +0100

    Top Paying Job

--- project_sql/1_top_paying_job.sql
+++ project_sql/1_top_paying_job.sql
@@ -4,3 +4,16 @@
  - focus on the jib posting with specific salaries 
  - why Highlight the top-paying opportynities for data analys offering insights into employment 
  */
+SELECT job_id,
+    job_title,
+    job_schedule_type,
+    salary_year_avg,
+    job_posted_date,
+    name AS company_name
+FROM job_postings_fact
+    LEFT JOIN company_dim ON job_postings_fact.company_id = company_dim.company_id
+WHERE job_title_short = 'Data Analyst'
+    AND salary_year_avg IS NOT NULL
+    AND job_location = 'Anywhere'
+ORDER BY salary_year_avg DESC
+LIMIT 10;
