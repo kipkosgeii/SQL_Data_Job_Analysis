bc853aba05076ac43a1bd17e7a1495e7330cfe46
Kipkosgeii <odkhoder@gmail.com>
2024-08-01 23:49:13 +0100

    Update Per country

--- project_sql/1_2_top_Jobs.sql
+++ project_sql/1_2_top_Jobs.sql
@@ -1,15 +1,15 @@
 /* 
- Question: What are the most top-jobs jobs?
+ Question: What are the most top-jobs according to countries?
  -Identify the top highest-jobs Data analyst roles that are availabe
  - focus on the jib posting with specific salaries 
  - why Highlight the top-paying opportynities for data analys offering insights into employment 
  */
 SELECT job_title_short,
-    COUNT(job_id) AS available_roles
+    job_country,
+    COUNT(job_postings_fact.job_id) AS available_roles
 FROM job_postings_fact
-GROUP BY job_title_short
+    INNER JOIN skills_job_dim ON job_postings_fact.job_id = skills_job_dim.job_id
+    INNER JOIN skills_dim ON skills_job_dim.skill_id = skills_dim.skill_id
+GROUP BY job_title_short,
+    job_country
 ORDER BY available_roles DESC
-LIMIT 10
-SELECT *
-FROM skills_dim
-limit 10;
